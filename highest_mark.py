49bf90f2b5761fe2a8f52b39884945d4e7e32ba8
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-21 21:56:41 +0530

    highest_marks.py

--- highest_mark.py
+++ highest_mark.py
@@ -2,17 +2,15 @@
 student={"Rohan":87,
          "sonam":78,
          "Rahul":81,
+         "irfan":89,
          "Samir":67,
-         "Aman":46,
-         "irfan":89
+         "Aman":46
          }
 # all_values=student.values()
 highest=student.get("Rohan")
-name1="Rohan"
-print(name1)
 for name,marks in  student.items() :
-   
     if marks > highest:
-        highest=marks
-print(f"{name1} = {highest}")        
+        highest = marks
+        highest_name = name
+print(f"{highest_name} = {highest}")        
 
