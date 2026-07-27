77f08a206414bd22f40822f5a351d334f18e8ea3
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-27 14:51:13 +0530

    practice8.py

---
+++ practice8.py
@@ -0,0 +1,10 @@
+# print all student 
+student={"roll 1":"Arshika",
+         "roll 2":"Rohan",
+         "roll 3":"Raman",
+         "roll 4":"Arshu",
+         "roll 5":"Karan",
+         "roll 6":"Arjun"}
+print(type(student))
+all_student=student.values()
+print(f"all student {all_student}")
