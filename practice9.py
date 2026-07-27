0a983c315e22d71792e5de5cdc0a175cac98be8a
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-27 14:59:39 +0530

    practice9.py

---
+++ practice9.py
@@ -0,0 +1,9 @@
+# count total student
+student={"roll 1":"Arshika",
+         "roll 2":"Rohan",
+         "roll 3":"Raman",
+         "roll 4":"Arshu",
+         "roll 5":"Karan",
+         "roll 6":"Arjun"}
+count=len(student)
+print(f"count all  student {count}\n{student}")
