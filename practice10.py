6903d3c76e54bd732443a7c29e76c255e12a25f3
Arshika kumari <arshikakumari2005@gmail.com>
2026-07-30 17:55:41 +0530

    practice10.py

--- practice10.py
+++ practice10.py
@@ -6,16 +6,18 @@ student={"arshika":85.80,
          "suman":91.0,
          "Arjun":86.78}
 highest=0
-
+highest_student_name=""
 lowest=100
+lowest_student_name=""
+
 for key,value in student.items():
     if highest < value:
         highest=value
-        print(key)
+        student_name=key
     elif lowest > value:
-        lowest=value  
-        store=key
-       
-print("highest marks =",key,highest) 
-print("lowest marks =",key,lowest)  
+        lowest =value
+        lowest_student_name=key
+
+print("highest marks =",student_name,highest) 
+print("lowest marks =",lowest_student_name,lowest)  
 
